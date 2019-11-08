7bbcd203d0299cda211e8542feb73381612e81e8
inclde123456@163.com <gaowei@sinovatech.com>
2019-11-08 17:38:39 +0800

    增加列表

---
+++ src/components/TrainList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <van-row   type="flex" justify="center">
+            <van-col v-for="item in items"  v-bind:key="item.key">{{item.images}}</van-col>
+        </van-row>
+        <van-row  v-for="item in items" type="flex" justify="center" v-bind:key="item.key">
+            <van-col  >{{item.images}}</van-col>
+        </van-row>
+    </div>
+
+</template>
+<script>
+    export default {
+        name:"TrainList",
+        data() {
+            return {
+                items: "[{images:123,size:123},{images:234,size:456}]"
+            }
+        }, methods: {
+            onSearch() {
+                this.$http.get('http://yapi.demo.qunar.com/mock/64532/bbyy_employee/user/aboutAs').then(result => {
+                    result.toString()
+                    this.items = result;
+                }), err => {
+                    alert(err);
+                }
+
+            }
+
+        },
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <IndexPage></IndexPage>
     <Banner></Banner>
+    <TrainList></TrainList>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue'
 import IndexPage from '@/components/IndexPage.vue'
+import TrainList from '@/components/TrainList.vue'
 export default {
   name: 'Home',
   components: {
     Banner,
-    IndexPage
+    IndexPage,
+    TrainList
   }
 }
 </script>
