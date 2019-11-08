a59d473a1d998f0d62758678629c25a67d8b1b23
inclde123456@163.com <gaowei@sinovatech.com>
2019-11-08 17:42:22 +0800

    增加列表

--- src/components/TrainList.vue
+++ src/components/TrainList.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <van-row   type="flex" justify="center">
-            <van-col v-for="item in items"  v-bind:key="item.key">{{item.images}}</van-col>
+            <van-col v-for="item in items"  v-bind:key="item.key">{{item}}</van-col>
         </van-row>
         <van-row  v-for="item in items" type="flex" justify="center" v-bind:key="item.key">
-            <van-col  >{{item.images}}</van-col>
+            <van-col  >{{item}}</van-col>
         </van-row>
     </div>
 
@@ -14,13 +14,12 @@
         name:"TrainList",
         data() {
             return {
-                items: "[{images:123,size:123},{images:234,size:456}]"
+                items: ['a', 'b', 'c']
             }
         }, methods: {
             onSearch() {
                 this.$http.get('http://yapi.demo.qunar.com/mock/64532/bbyy_employee/user/aboutAs').then(result => {
                     result.toString()
-                    this.items = result;
                 }), err => {
                     alert(err);
                 }
